f2bcd4f950fa3b0d5b3f7d7a06144d569addbe55
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2016-11-28 23:05:08 +0300

    added selenium and geckodriver

--- run_tests.py
+++ run_tests.py
@@ -1,24 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import sys
-
-from tests.car_showrooms.add_showroom_test import AddShowroomFormTest
-from tests.car_showrooms.list_showroom_test import ShowroomListTest
-from tests.car_showrooms.list_special_offers_test import SpecialOffersListTest
-from tests.car_showrooms.search_showroom_tests import *
+from selenium import webdriver
 
 if __name__ == '__main__':
-
-    suite = unittest.TestSuite((
-        unittest.makeSuite(RegionSelectFormTest),
-        unittest.makeSuite(SelectCarModelTest),
-        unittest.makeSuite(SelectStationTest),
-        unittest.makeSuite(IsOfficialCheckboxTest),
-        unittest.makeSuite(SearchFormTest),
-        unittest.makeSuite(ShowroomListTest),
-        unittest.makeSuite(SpecialOffersListTest),
-        unittest.makeSuite(AddShowroomFormTest),
-    ))
-    result = unittest.TextTestRunner().run(suite)
-
-    sys.exit(not result.wasSuccessful())
+    driver = webdriver.Firefox(executable_path='./geckodriver')
+    driver.get("http://park.mail.ru/")
+    driver.quit()
