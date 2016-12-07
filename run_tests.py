be68cc703fd51150abba5c77574e5331230e675a
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2016-12-07 11:02:59 +0300

    updated

--- run_tests.py
+++ run_tests.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import sys, getopt
+import unittest
 from selenium import webdriver
+from tests.group_messages_test import GroupMessagesTest
 
 if __name__ == '__main__':
-    driver = webdriver.Firefox(executable_path='./geckodriver')
-    driver.get("http://park.mail.ru/")
-    driver.quit()
+    suite = unittest.TestSuite((
+         unittest.makeSuite(GroupMessagesTest),
+    ))
+    result = unittest.TextTestRunner().run(suite)
+    sys.exit(not result.wasSuccessful())
