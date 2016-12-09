87f13ff77782a82072bdd8e912492180bc6d60b8
Nick Shch <ndnd@mail.ru>
2016-12-09 21:25:31 +0300

    Refactoring

--- videopreview_test.py
+++ videopreview_test.py
@@ -6,32 +6,25 @@ from pages.video_page import VideoPage
 
 class VideoPreviewTest(BaseCase):
 
+    def setUp(self):
+        super(VideoPreviewTest, self).setUp()
+        self.videos_page = SuggestedVideos(self.driver)
+        self.videos_page.open()
+        self.first_video_link = self.videos_page.open_first_video()
+
     def test_openclose(self):
-        videos_page = SuggestedVideos(self.driver)
-        videos_page.open()
-
-        video_link = videos_page.open_first_video()
-        self.assertIn(video_link, self.driver.current_url)
-
+        self.assertIn(self.first_video_link, self.driver.current_url)
         videoplayer_page = VideoPage(self.driver, self.driver.current_url)
         videoplayer_page.close_video()
-        self.assertIn(videos_page.PATH, self.driver.current_url)
+        self.assertIn(self.videos_page.PATH, self.driver.current_url)
 
     def test_open_in_newtab(self):
-        videos_page = SuggestedVideos(self.driver)
-        videos_page.open()
-        videos_page.open_first_video()
-
         videoplayer_page = VideoPage(self.driver, self.driver.current_url)
         url_related_video = videoplayer_page.get_url_related_video()
         videoplayer_page.open_related_video_in_new_tab()
         self.assertEqual(url_related_video, self.driver.current_url) #bug in ok.ru here
 
     def test_video_plays(self):
-        videos_page = SuggestedVideos(self.driver)
-        videos_page.open()
-        videos_page.open_first_video()
-
         videoplayer_page = VideoPage(self.driver, self.driver.current_url)
         videoplayer_page.play_video()
         time1 = videoplayer_page.get_video_play_time()
@@ -40,10 +33,6 @@ class VideoPreviewTest(BaseCase):
         self.assertNotEqual(time1, time2)
 
     def test_video_pauses(self):
-        videos_page = SuggestedVideos(self.driver)
-        videos_page.open()
-        videos_page.open_first_video()
-
         videoplayer_page = VideoPage(self.driver, self.driver.current_url)
         videoplayer_page.pause_video()
         time1 = videoplayer_page.get_video_play_time()
@@ -52,29 +41,17 @@ class VideoPreviewTest(BaseCase):
         self.assertEqual(time1, time2)
 
     def test_video_stops(self):
-        videos_page = SuggestedVideos(self.driver)
-        videos_page.open()
-        videos_page.open_first_video()
-
         videoplayer_page = VideoPage(self.driver, self.driver.current_url)
         videoplayer_page.stop_video()
         self.assertTrue(videoplayer_page.is_cover_visible())
 
     def test_next_video(self):
-        videos_page = SuggestedVideos(self.driver)
-        videos_page.open()
-        videos_page.open_first_video()
-
         videoplayer_page = VideoPage(self.driver, self.driver.current_url)
         videoplayer_page.play_next_video()
         nextvideo_page = VideoPage(self.driver, self.driver.current_url)
         self.assertNotEqual(videoplayer_page.PATH, nextvideo_page.PATH)
 
     def test_video_rewind(self):
-        videos_page = SuggestedVideos(self.driver)
-        videos_page.open()
-        videos_page.open_first_video()
-
         videoplayer_page = VideoPage(self.driver, self.driver.current_url)
         videoplayer_page.pause_video()
         begin_time = videoplayer_page.get_video_play_time()
