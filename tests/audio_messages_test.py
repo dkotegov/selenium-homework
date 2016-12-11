aa3c7fbbc6a2dd7099c9b0d58b0bbbffd02d3358
Edgar Nurullin <edgar.nurullin@onetwotrip.com>
2016-12-12 02:52:34 +0300

    refactoring

--- tests/audio_messages_test.py
+++ tests/audio_messages_test.py
@@ -20,7 +20,7 @@ class GroupMessagesPage(Page):
 class MessagesMenu(Component):
     ATTACH_BUTTON_TRIG = '//span[@class="comments_attach_trigger"]'
     ATTACH_VIDEOMSG_BUTTON = '//span[@class="comments_attach_trigger"]/div[2]/div/div/ul/li[1]'
-    VIDEOMSG_POPUP = '//span[@id="hook_Modal_popLayerModal"]'
+    VIDEOMSG_POPUP = '//object[@class="vchat_flash_app"]'
 
     def get_button_attach(self):
         WebDriverWait(self.driver, 30, 0.1).until(
@@ -39,6 +39,11 @@ class MessagesMenu(Component):
             lambda d: d.find_element_by_xpath(self.VIDEOMSG_POPUP)
         )
 
+    def click_play_button(self):
+        WebDriverWait(self.driver, 30, 0.1).until(
+            lambda d: d.find_element_by_xpath(self.ATTACH_VIDEOMSG_BUTTON)
+        )
+        self.driver.find_element_by_xpath(self.ATTACH_VIDEOMSG_BUTTON).click()
 
 class AudioMessagesTest(unittest.TestCase):
     USERS_COUNT = u'3 участника'
@@ -54,16 +59,22 @@ class AudioMessagesTest(unittest.TestCase):
         auth_page = AuthPage(self.driver)  # Auth here
         auth_page.authorize()
 
+        self.message_page = GroupMessagesPage(self.driver)
+
     def tearDown(self):
         self.driver.quit()
 
-    def test_videomessage_window_opens(self):
-        message_page = GroupMessagesPage(self.driver)  # Go to messages
-        message_page.open()
+    def test_audiomessage_window_opens(self):
+         # Go to messages
+        self.message_page.open()
+        self.message_page.messages_menu.get_button_attach()
 
-        message_page.messages_menu.get_button_attach()
+        self.message_page.messages_menu.get_button_videomessage()
 
-        message_page.messages_menu.get_button_videomessage()
-
-        videomsg_popup = message_page.messages_menu.get_videomessage_popup()
+        videomsg_popup = self.message_page.messages_menu.get_videomessage_popup()
+        #проверяем выскочил ли object с флешом
         self.assertIsNotNone(videomsg_popup)
+
+    def test_audiomessage_play_message(self):
+        self.message_page.open()
+        self.message_page.messages_menu.click_play_button()
