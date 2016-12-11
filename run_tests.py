d0f11f9dfb13cdfdaccfce9d97f08d4dd7a9bdce
Edgar Nurullin <edgar.nurullin@onetwotrip.com>
2016-12-12 00:09:22 +0300

    audio test

--- run_tests.py
+++ run_tests.py
@@ -3,10 +3,13 @@
 import sys, getopt
 import unittest
 from tests.group_messages_test import GroupMessagesTest
+from tests.audio_messages_test import AudioMessagesTest
+from tests.video_messages_test import VideoMessagesTest
+
 
 if __name__ == '__main__':
     suite = unittest.TestSuite((
-         unittest.makeSuite(GroupMessagesTest),
+         unittest.makeSuite(AudioMessagesTest),
     ))
     result = unittest.TextTestRunner().run(suite)
     sys.exit(not result.wasSuccessful())
