371402b667319e90f347e31fc51794f558af4f07
pavelrpaul <pavel.rukavishnikov@gmail.com>
2016-12-12 23:10:22 +0300

    fix: part 2.5.6

--- run_tests.py
+++ run_tests.py
@@ -5,11 +5,12 @@ import unittest
 from tests.group_messages_test import GroupMessagesTest
 from tests.audio_messages_test import AudioMessagesTest
 from tests.video_messages_test import VideoMessagesTest
+from tests.simple_messages_test import SimpleMessagesTest
 
 
 if __name__ == '__main__':
     suite = unittest.TestSuite((
-         unittest.makeSuite(AudioMessagesTest),
+         unittest.makeSuite(AudioMessagesTest),unittest.makeSuite(SimpleMessagesTest),
     ))
     result = unittest.TextTestRunner().run(suite)
     sys.exit(not result.wasSuccessful())
--- tests/simple_messages_test.py
+++ tests/simple_messages_test.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import unittest
 import settings
@@ -9,7 +10,7 @@ from selenium.webdriver import DesiredCapabilities, Remote
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-class GroupMessagesPage(Page):
+class SimpleMessagesPage(Page):
     BASE_URL = 'https://ok.ru/messages/589325601321'
 
     @property
@@ -18,18 +19,24 @@ class GroupMessagesPage(Page):
 
 
 class MessagesMenu(Component):
-    ATTACH_BUTTON_TRIG = '//span[@class="comments_attach_trigger"]'
-    ATTACH_AUDIO_MSG_BUTTON = '//span[@class="comments_attach_trigger"]/div[2]/div/div/ul/li[1]'
-    AUDIO_MSG_POPUP = '//object[@class="vchat_flash_app"]'
-    PLAY_BUTTON = '//div[@class="msg_audio"]/div[@class="msg_audio_play"]/'#/div[last()]/div[@class="msg_cnt"]'#/div[]/div[@class="js-msg-attach"]/div/div'
+    SEND_MSG_BTN = '//button[@class="button-pro comments_add-controls_save"]'
+    MESSAGE = '//div[@name="st.txt"]'
 
-    def get_users_count(self):
-        return WebDriverWait(self.driver, 30, 0.1).until(
-            lambda d: d.find_element_by_xpath(self.USERS_COUNT).text
+    # ATTACH_BUTTON_TRIG = '//span[@class="comments_attach_trigger"]'
+    # ATTACH_AUDIO_MSG_BUTTON = '//span[@class="comments_attach_trigger"]/div[2]/div/div/ul/li[1]'
+    # AUDIO_MSG_POPUP = '//object[@class="vchat_flash_app"]'
+    # PLAY_BUTTON = '//div[@class="msg_audio"]/div[@class="msg_audio_play"]/'#/div[last()]/div[@class="msg_cnt"]'#/div[]/div[@class="js-msg-attach"]/div/div'
+
+    def get_button_send(self):
+        WebDriverWait(self.driver, 30, 0.1).until(
+            lambda d: d.find_element_by_xpath(self.SEND_MSG_BTN)
         )
+        self.driver.find_element_by_xpath(self.SEND_MSG_BTN).click()
+
+    def set_message(self, message):
+        self.driver.find_element_by_xpath(self.MESSAGE).send_keys(message)
 
 class SimpleMessagesTest(unittest.TestCase):
-    USERS_COUNT = u'2 участника'
 
     def setUp(self):
         browser = os.environ.get('BROWSER', 'FIREFOX')
@@ -42,6 +49,16 @@ class SimpleMessagesTest(unittest.TestCase):
         auth_page = AuthPage(self.driver)  # Auth here
         auth_page.authorize()
 
-        self.message_page = GroupMessagesPage(self.driver)
+        self.message_page = SimpleMessagesPage(self.driver)
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_send_simple_message(self):
+        rand = random.random() * 10**10
+        message = str(rand)
+        self.message_page.open()
+        self.message_page.messages_menu.set_message(message)
+        self.message_page.messages_menu.get_button_send()
 
 
