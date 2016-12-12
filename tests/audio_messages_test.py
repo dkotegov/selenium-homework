c63ecece33268d3d06cb95acfcc0f612a1263bd1
Edgar Nurullin <edgar.nurullin@onetwotrip.com>
2016-12-12 19:41:02 +0300

    hard

--- tests/audio_messages_test.py
+++ tests/audio_messages_test.py
@@ -3,6 +3,7 @@ import os
 
 import unittest
 import settings
+from time import sleep
 from BasicMethods import Page, Component, AuthPage
 
 from selenium.webdriver import DesiredCapabilities, Remote
@@ -21,7 +22,7 @@ class MessagesMenu(Component):
     ATTACH_BUTTON_TRIG = '//span[@class="comments_attach_trigger"]'
     ATTACH_VIDEOMSG_BUTTON = '//span[@class="comments_attach_trigger"]/div[2]/div/div/ul/li[1]'
     VIDEOMSG_POPUP = '//object[@class="vchat_flash_app"]'
-    PLAY_BUTTON = '//div[@class="js-messages-list"]/div/div[last()]/div[2]/div/div[@class="js-msg-attach"]'#/div[]/div[@class="js-msg-attach"]/div/div'
+    PLAY_BUTTON = '//div[@class="msg_audio"]/div[@class="msg_audio_play"]/'#/div[last()]/div[@class="msg_cnt"]'#/div[]/div[@class="js-msg-attach"]/div/div'
     def get_button_attach(self):
         WebDriverWait(self.driver, 30, 0.1).until(
             lambda d: d.find_element_by_xpath(self.ATTACH_BUTTON_TRIG)
@@ -41,12 +42,9 @@ class MessagesMenu(Component):
 
     def click_play_button(self):
         WebDriverWait(self.driver, 30, 0.1).until(
-            lambda d: d.find_element_by_xpath(self.PLAY_BUTTON)
+            lambda d: d.find_element_by_css_selector('.msg_audio:last-child')
         )
-        lal = self.driver
-        df = lal.find_element_by_xpath(self.PLAY_BUTTON)
-        self.driver.find_element_by_xpath(self.PLAY_BUTTON).click()
-        self.driver.implicitly_wait(10)  # seconds
+
 
 class AudioMessagesTest(unittest.TestCase):
     USERS_COUNT = u'3 участника'
@@ -67,9 +65,9 @@ class AudioMessagesTest(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    def test_audiomessage_window_opens(self):
+    #def test_audiomessage_window_opens(self):
          # Go to messages
-        self.message_page.open()
+        #self.message_page.open()
         # self.message_page.messages_menu.get_button_attach()
         #
         # self.message_page.messages_menu.get_button_videomessage()
@@ -81,3 +79,14 @@ class AudioMessagesTest(unittest.TestCase):
     def test_audiomessage_play_message(self):
         self.message_page.open()
         self.message_page.messages_menu.click_play_button()
+        last_audio_msg = self.driver.find_element_by_css_selector('.msg_audio:last-child')
+        dfdf = self.driver.find_elements_by_xpath('//div[@class="msg_audio"]')
+        aaaaaaa = last_audio_msg.find_element_by_xpath('//div[@class="msg_audio_play"]')
+        WebDriverWait(self.driver, 30, 0.1).until(
+            lambda d: self.driver.find_elements_by_xpath('//div[@class="msg_audio_play"]')
+        )
+
+        aaaaaaa.click()
+        #sleep(1)
+        classname_last_audio_msg = last_audio_msg.get_attribute("class")
+        self.assertTrue(classname_last_audio_msg.rfind("st_play") != -1)
