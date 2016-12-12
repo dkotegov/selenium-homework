f7cb495cc6737db2fce4cfdd67e44f369d145ed2
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2016-12-13 01:55:34 +0300

    group messages refactored

--- tests/group_messages_test.py
+++ tests/group_messages_test.py
@@ -25,6 +25,21 @@ class MessagesMenu(Component):
             lambda d: d.find_element_by_xpath(self.USERS_COUNT).text
         )
 
+    def send_message(self):
+        self.driver.find_element_by_css_selector('.itx').send_keys("Тестовое сообщение")
+        self.driver.find_element_by_css_selector('.comments_add-controls_save').click()
+
+    def get_text_from_input(self):
+        return self.driver.find_element_by_css_selector('.itx').text
+
+    def report_msg(self):
+        self.driver.find_element_by_css_selector('.ic10_warn-g').click()
+
+    def get_report_popup(self):
+        return WebDriverWait(self.driver, 30, 0.1).until(
+            lambda d: d.find_element_by_css_selector('.modal-new_cnt')
+        )
+
 
 class GroupMessagesTest(unittest.TestCase):
     USERS_COUNT = u'3 участника'
@@ -44,8 +59,20 @@ class GroupMessagesTest(unittest.TestCase):
         self.driver.quit()
 
     def test(self):
+
         message_page = GroupMessagesPage(self.driver)  # Go to messages
         message_page.open()
 
-        users_count = message_page.messages_menu.get_users_count()
+        users_count = message_page.messages_menu.get_users_count() # проверка на правильное количество участников
         self.assertEqual(self.USERS_COUNT, users_count)
+
+        message_page.messages_menu.send_message()
+        self.assertEqual(message_page.messages_menu.get_text_from_input(), '')  # отправка сообщений
+
+        message_page.messages_menu.report_msg()
+        report_window = message_page.messages_menu.get_report_popup()
+        self.assertTrue(report_window, not None) # проверка появления попапа о репорте
+
+
+
+
